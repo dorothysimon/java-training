31c5653b20696af10f68330eb5b2eb46de5b962f
sdoro <simon.b.dorottya@gmail.com>
2024-09-30 14:28:44 +0200

    playing with methods

--- src/main/java/training/Trainer.java
+++ src/main/java/training/Trainer.java
@@ -1,6 +1,30 @@
 package training;
 
 public class Trainer {
-    String name;
+    private String name;
     int yearOfBirth;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getYearOfBirth(){
+        return yearOfBirth;
+    }
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getNameAndYearOfBirth(){
+        return name+ " "+yearOfBirth;
+    }
+    public int getAge(int year){
+        return year-yearOfBirth;
+    }
+    public void changeName(String newName){
+        name = newName;
+    }
+
 }
--- src/main/java/training/TrainerMain.java
+++ src/main/java/training/TrainerMain.java
@@ -3,15 +3,29 @@ package training;
 public class TrainerMain {
     public static void main(String[] args) {
         Trainer trainer = new Trainer();
-        trainer.name = "John Doe";
-        trainer.yearOfBirth = 1980;
-        System.out.println(trainer.name);
-        System.out.println(trainer.yearOfBirth);
+        trainer.setName("John Doe");
+        trainer.setYearOfBirth(1980);
+        System.out.println(trainer.getName());
+        System.out.println(trainer.getYearOfBirth());
 
         Trainer anotherTrainer = new Trainer();
-        anotherTrainer.name = "Jack Doe";
-        System.out.println(anotherTrainer.name);
+        anotherTrainer.setName("Jack Doe");
+        System.out.println(anotherTrainer.getName());
 
+        System.out.println("With methods:");
+
+        System.out.println(trainer.getName());
+        System.out.println("Age of "+trainer.getName() + " is " + trainer.getAge(2024));
+
+        System.out.println(anotherTrainer.getNameAndYearOfBirth());
+        anotherTrainer.setYearOfBirth(1990);
+        System.out.println(anotherTrainer.getNameAndYearOfBirth());
+
+        anotherTrainer.changeName("Jason Doe");
+        System.out.println(anotherTrainer.getName());
+
+        anotherTrainer.setName("Jack Doe");
+        System.out.println(anotherTrainer.getName());
 
     }
 }
